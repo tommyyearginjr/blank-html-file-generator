ee0832cd82b27118388802f0d6f511f4ce66f256
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-03-20 09:41:54 -0400

    Revised the prompt, and added a response message after page is created.

--- blankFile.py
+++ blankFile.py
@@ -1,3 +1,4 @@
 from pathlib import Path
-fileName = raw_input('Name the file. Don\'t forget to add suffix! : ')
+fileName = raw_input('Name the webpage you want to create. Don\'t forget to add suffix! : ')
 Path(fileName).touch()
+print("The webpage " + fileName + " was created. Live long and prosper.")
