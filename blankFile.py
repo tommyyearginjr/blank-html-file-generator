3ca07b544d22e0afe0a880b159117feafdfd530e
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-03-20 09:46:16 -0400

    added spaces to blankFile.py, for clarity.

--- blankFile.py
+++ blankFile.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+
 fileName = raw_input('Name the webpage you want to create. Don\'t forget to add suffix! : ')
+
 Path(fileName).touch()
+
 print("The webpage " + fileName + " was created. Live long and prosper.")
