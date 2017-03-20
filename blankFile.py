0635cdf145ec67ee852bb64d95563af1d236e239
Tommy H. Yeargin, Jr <tommy.yeargin@gmail.com>
2017-03-20 10:44:34 -0400

    removed blankFile.py and htmlGen.py.

--- blankFile.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-fileName = raw_input('Name the webpage you want to create. Don\'t forget to add suffix! : ')
-
-Path(fileName).touch()
-
-print("The webpage " + fileName + " was created. Live long and prosper.")
--- htmlGen.py
+++
@@ -1,3 +0,0 @@
-blankHTML = ['<!DOCTYPE html>', '<html>', "     <head>", '          <title>', '          </title>', '          <style>', '          </style>', '          <script>', '          </script>', '     </head>', '     <body>', '          <script>', '          </script>', '     </body>', '</html>']
-for item in blankHTML:
-    print item
